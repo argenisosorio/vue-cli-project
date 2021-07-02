15fbf5c68e73188d2cb32926020b9c9f268a61e1
Argenis Osorio <argenisosorio580@hotmail.com>
2021-07-02 01:13:39 -0400

    Added example of 2 components

---
+++ src/components/AppContent.vue
@@ -0,0 +1,17 @@
+<template>
+  <li>{{ prop.title }}</li>
+</template>
+<script>
+export default {
+  props: ['prop'],
+  data() {
+    return {
+      bookList: [
+        { title: 'Il cimpendium malefican' },
+        { title: 'Free demololatrie' },
+        { title: 'The nine gate' }
+      ]  
+    }
+  }
+}
+</script>
