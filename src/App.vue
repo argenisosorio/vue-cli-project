ff160376b13bfac391abd65b97c44bbd88397730
Argenis Osorio <argenisosorio580@hotmail.com>
2021-09-01 02:26:01 -0400

    Added example of axios use

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,47 @@
 <template>
-  <div>
-    {{ message }}
+  <div id="main" class="container">
+    <div class="row">
+      <div class="col-12">
+        <table border="1px">
+          <thead>
+            <tr>
+              <th scope="col">First Name</th>
+              <th scope="col">Age</th>
+              <th scope="col">Picture</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(usuario, index) in listaUsuarios" v-bind:key="index">
+              <td v-text="usuario.location.street.name"></td>
+              <td v-text="usuario.dob.age"></td>
+              <td>
+                <img v-bind:src="usuario.picture.thumbnail" v-bind:alt="usuario.picture.thumbnail" class="rounded-circle" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'App',
   data() {
     return {
-      message: 'Hello world!'
+      listaUsuarios: []
+    }
+  },
+  created: function(){
+    this.consultarUsuarios();
+  },
+  methods: {
+    consultarUsuarios: function() {
+      axios.get('https://randomuser.me/api/?results=3').then(response => {
+        this.listaUsuarios = response.data.results
+      });
     }
   }
 }
